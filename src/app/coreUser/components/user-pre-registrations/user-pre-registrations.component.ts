21687aaa41c1712823b5c0cd4d08e8ab635b35e9
David Ceballos Mata <davcm24@hotmail.com>
2024-09-17 00:03:46 -0600

    changes

--- src/app/coreUser/components/user-pre-registrations/user-pre-registrations.component.ts
+++ src/app/coreUser/components/user-pre-registrations/user-pre-registrations.component.ts
@@ -27,17 +27,6 @@ export class UserPreRegistrationsComponent implements OnInit {
     this.getPreRegistrations();
   }
 
-  /*formatDate(date: any): string {
-    if (!date) return '';
-    const d = new Date(date);
-
-    const year = d.getFullYear();
-    const month = String(d.getMonth() + 1).padStart(2, '0'); 
-    const day = String(d.getDate()).padStart(2, '0');
-
-    return `${day}-${month}-${year}`;
-  }*/
-
   formatDate(date: any): string {
     if (!date || !Array.isArray(date)) return 'Fecha no válida';
   
